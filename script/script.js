3e8a2a7f9d708fb82dfec1a724cb6351a2fed972
martin9623 <martingomez.9623@gmail.com>
2021-11-23 18:59:29 -0300

    Agregado proyecto Embragues Union al portafolio

--- script/script.js
+++ script/script.js
@@ -2,15 +2,12 @@ const navBtn = document.querySelector(".nav-btn");
 const menu = document.querySelector(".menu");
 const linksMenu = document.querySelectorAll(".menu__item-link");
 
-function menuToggle(x) {
-  x.addEventListener("click", () => {
+function menuToggle(btn) {
+  btn.addEventListener("click", () => {
     menu.classList.toggle("menu-desactivado");
   });
 }
 
 menuToggle(navBtn);
-menuToggle(linksMenu[0]);
-menuToggle(linksMenu[1]);
-menuToggle(linksMenu[2]);
-menuToggle(linksMenu[3]);
-menuToggle(linksMenu[4]);
+
+linksMenu.forEach((link) => menuToggle(link));
